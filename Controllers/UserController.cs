8761947682d07cf7df8d007f4afdd073c3d03881
amand <amand@DESKTOP-1B8UARM>
2022-08-16 19:12:23 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeraApi.Model;
+
+namespace PrimeraApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet(Name = "GetUsers")]
+        public List<User> GetUsers()
+        {
+            return new List<User>() { new User { Id = 1, Name = "Agus" } };
+        }
+    }
+}
